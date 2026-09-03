ef423bf09f05efebc9cd6f9a49618455e9c9716c
Sumit Sarswat <sumitsarswat833@gmail.com>
2026-09-03 05:36:37 +0530

    project live

--- database/database.sql
+++ database/database.sql
@@ -1,6 +1,3 @@
-CREATE DATABASE IF NOT EXISTS `college_admission_python`;
-USE `college_admission_python`;
-
 -- --------------------------------------------------------
 -- Table structure for `admins`
 -- --------------------------------------------------------
